d79f91845f9c6d974ad4e23f78790ca786831cdd
Mohammed <mohammedsouleymane@outlook.com>
2022-12-03 11:28:02 +0100

    day 3

---
+++ AdventOfCode2022/AdventOfCode2022/Day03.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2022;
+
+public static class Day03
+{
+	private static readonly List<string> Rucksacks = FileReader.GetData("/day03/in.txt");
+
+	private const int Lower = 96;
+	private const int Upper = 38;
+
+
+	public static IEnumerable<char> DuplicatedItems()
+	{
+		var distinctCompartments = Rucksacks.Select(x =>new string(x[0..Mid(x)].Distinct().ToArray()) + new string(x[Mid(x)..].Distinct().ToArray()));
+		return distinctCompartments.Select(x => x.GroupBy(c=> c).First(y=> y.Count() > 1).Key);
+	}
+
+	public static List<char> BadgeItem()
+	{
+		var badgeItems = new List<char>();
+		for (var i = 0; i < Rucksacks.Count; i+=3)
+		{
+			var group = Rucksacks.Skip(i).Take(3).Select(x=> new string (x.Distinct().ToArray()));
+			var badgeItem = string.Join("",group).GroupBy(x=> x).Where( y=> y.Count() > 2);
+			badgeItems.Add(badgeItem.First().Key);
+		}
+
+		return badgeItems;
+	}
+
+	public static int Sum => CalSum(DuplicatedItems());
+
+	public static int SumBadgeItems => CalSum(BadgeItem());
+	private static int Mid (string x) { return x.Length/2; }
+
+	private static int CalSum(IEnumerable<char> ls)
+	{
+		return ls.Sum(x => char.IsLower(x) ? x - Lower : x - Upper);
+	}
+}
--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine(Day01.Max);
 Console.WriteLine(Day01.Top3);
 Console.WriteLine(Day02.PartOne());
 Console.WriteLine(Day02.PartTwo());
+Console.WriteLine(Day03.SumOfDuplicates);
+Console.WriteLine(Day03.SumPrioritiesOfBadgeItems);
