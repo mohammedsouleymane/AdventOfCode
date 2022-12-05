f06e2e272099909d1b766f19fefb3d2758caf237
Mohammed <mohammedsouleymane@outlook.com>
2022-12-05 09:18:01 +0100

    optimize day 5

--- AdventOfCode2022/AdventOfCode2022/Day05.cs
+++ AdventOfCode2022/AdventOfCode2022/Day05.cs
@@ -2,17 +2,12 @@
 
 public static class Day05
 {
-    private static readonly List<string> StacksAndMoves = FileReader.GetData("/day05/input");
+    private static readonly List<string> StacksAndMoves = Util.ReadFile("/day05/input");
     private static List<string>? _moves;
+    public static string TopCratesWithMover9000 => Rearrange().Select(x => x.First()).ToStr();
+    public static string TopCratesWithMover9001 => Rearrange(true).Select(x => x.First()).ToStr();
 
-    public static string GetTopCrates(bool mover9001 = false)
-    {
-        var stacks = Rearrange(mover9001);
-        var crates = stacks.Select(x => x.First().Replace("[", "").Replace("]", ""));
-        return string.Join("", crates);
-    }
-
-    private static Stack<string>[] Rearrange(bool mover9001 = false)
+    private static IEnumerable<Stack<char>> Rearrange(bool mover9001 = false)
     {
         var stacks = GetStacks();
         foreach (var move in _moves!.Select(x=> x.Trim().Split(" ")))
@@ -20,7 +15,7 @@ public static class Day05
             var take = int.Parse(move[1]);
             var from = int.Parse(move[3]) - 1;
             var to = int.Parse(move[5]) - 1;
-            var stack = stacks![from].Take(take).ToArray();
+            var stack = stacks[from].Take(take).ToArray();
             if (mover9001)
                 stack = stack.Reverse().ToArray();
             
@@ -33,27 +28,20 @@ public static class Day05
         return stacks;
     }
 
-    private static Stack<string>[] GetStacks()
+    private static Stack<char>[] GetStacks()
     {
-        var stacks = new List<Stack<string>>();
         var rows = StacksAndMoves.TakeWhile(x => x != "").ToList();
-        var stackCount = int.Parse(rows.Last().Trim().Split(" ").Last());
         _moves = StacksAndMoves.Skip(rows.Count + 1).ToList();
-        rows.RemoveAt(stackCount-1);
-        for (var i = 0; i < stackCount; i++)
-        {
-            stacks.Add(GetStack(rows, i*4));
-        }
-        return  stacks.ToArray();
+        return  rows.Select((_, i) => GetStack(rows, i * 4)).ToArray();
     }
 
-    private static Stack<string> GetStack(IEnumerable<string> rows, int index)
+    private static Stack<char> GetStack(IEnumerable<string> rows, int index)
     {
-        var stack = new Stack<string>();
-        foreach (var row in rows.Reverse())
+        var stack = new Stack<char>();
+        foreach (var row in rows.SkipLast(1).Reverse())
         {
-            var crate = new string(row.Skip(index).Take(3).ToArray());
-            if(crate != "   " && crate != "")
+            var crate = row[index+1];
+            if(crate != ' ')
                 stack.Push(crate);
         }
         return stack;
--- AdventOfCode2022/AdventOfCode2022/Util.cs
+++ AdventOfCode2022/AdventOfCode2022/Util.cs
@@ -1,10 +1,15 @@
 ﻿namespace AdventOfCode2022;
 
-public static class FileReader
+public static class Util
 {
-	public static List<string> GetData(string path)
+	public static List<string> ReadFile(string path)
 	{
 		path = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent + "/input"+path;
 		return File.ReadAllLines(path).ToList();
+	}
+
+	public static string ToStr(this List<string> list)
+	{
+		return string.Join("", list);
 	}
 }
