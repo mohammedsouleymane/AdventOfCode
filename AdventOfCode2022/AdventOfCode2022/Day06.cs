0548f2bdcd99c4e01838b9b2cf76d3c74b89fc60
Mohammed <mohammedsouleymane@outlook.com>
2022-12-06 15:03:37 +0100

    day 6

---
+++ AdventOfCode2022/AdventOfCode2022/Day06.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2022;
+
+public class Day06
+{
+    private static readonly string DatastreamBuffers = Util.ReadFile("/day06/input").First();
+
+    public static readonly int StartOfPacketMarker = DatastreamBuffers.Select((_, i) => i)
+        .First(x => DatastreamBuffers[x..(x + 4)].Distinct().Count() == 4) + 4;
+
+    public static readonly int StartOfMessageMarker = DatastreamBuffers.Select((_, i) => i)
+        .First(x => DatastreamBuffers[x..(x + 14)].Distinct().Count() == 14) + 14;
+}
--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -11,3 +11,5 @@ Console.WriteLine(Day04.FullyOverlap);
 Console.WriteLine(Day04.PartialOverlap);
 Console.WriteLine(Day05.TopCratesWithMover9000);
 Console.WriteLine(Day05.TopCratesWithMover9001);
+Console.WriteLine(Day06.StartOfPacketMarker);
+Console.WriteLine(Day06.StartOfMessageMarker);
