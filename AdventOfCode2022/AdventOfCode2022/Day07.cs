24c1a44d148723e8e91b22a09a184e4b1c104ae5
Mohammed <mohammedsouleymane@outlook.com>
2022-12-08 23:37:51 +0100

    update day 7

--- AdventOfCode2022/AdventOfCode2022/Day07.cs
+++ AdventOfCode2022/AdventOfCode2022/Day07.cs
@@ -5,42 +5,26 @@ namespace AdventOfCode2022;
 
 public static class Day07
 {
-    private static IEnumerable<string> Lines = Util.ReadFile("/day07/input").Where(x=> !x.StartsWith("dir"));
-    private static Stack<string> Paths = new Stack<string>();
-    private static Dictionary<string, int> sizes = new Dictionary<string, int>();
-    private static readonly List<int> Sizes = GetDirectorySizes();
+    private static readonly IEnumerable<(string s, int i)> Lines = Util.ReadFile("/day07/input").Select((s, i) => (s,i));
+    private static readonly List<int> Sizes = new ();
+    private static readonly int Root = DirSize();
     public static readonly int SumOfTotalSizeMin100000 = Sizes.Where(x => x <= 100000).Sum();
-    public static readonly int SpaceToFreeUp = Sizes.Where(c => c + 70000000 - Sizes.Max() >= 30000000).Min();
+    public static readonly int SpaceToFreeUp = Sizes.Where(c => c + 70000000 - Root >= 30000000).Min();
+    private static int _linesToSkip = 0;
 
-
-
-
-    private static List<int> GetDirectorySizes()
+    private static int DirSize(int dir = 0)
     {
-        if(!Lines.Any())
-            return sizes.Values.ToList();
-        var line = Lines.First();
-        Lines = Lines.Skip(1);
-        if (line.Contains(".."))
-        {
-            Paths.Pop();
-        }
-        else if (line.StartsWith("$ cd"))
-        {
-            Lines = Lines.Skip(1);
-            Paths.Push(Paths.ToStr() + line.Split(" ")[2]);
-            GetDirectorySizes();
-        }
-        else if (char.IsDigit(line[0]))
-        {
-            foreach (var dir in Paths)
-            {
-                sizes[dir] = sizes.GetValueOrDefault(dir) + int.Parse(line.Split(" ")[0]);;
-            }
-            GetDirectorySizes();
-        }
-        if(Lines.Any())
-            GetDirectorySizes();
-        return sizes.Values.ToList();
+        var ls = Lines.Skip( dir ).Skip(2).TakeWhile(x => !x.s.StartsWith("$")).ToList();
+        var size = ls.Where(x => !x.s.Contains("dir")).Sum(x => int.Parse(x.s.Split(" ")[0]));
+        size += ls.Where(x => x.s.Contains("dir")).Sum(x => DirSize(Cd(x.i)));
+
+        _linesToSkip = dir > _linesToSkip ? dir : _linesToSkip;
+        Sizes.Add(size);
+        return size;
+    }
+    private static int Cd(int index)
+    {
+        var child = Lines.Take(index+1).Last().s.Split(" ")[1];
+        return Lines.Skip(_linesToSkip +1).First(x => x.s.Contains($"cd {child}")).i;
     }
 }
