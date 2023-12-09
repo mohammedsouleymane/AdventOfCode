d6ae76d2c1857fc1ab51d2d1fbd7a62422fc6283
geegee <mohammedsouleymane@outlook.com>
2023-12-09 15:29:25 +0100

    update nine

--- AdventOfCode/Aoc2023/Day09.cs
+++ AdventOfCode/Aoc2023/Day09.cs
@@ -5,17 +5,14 @@ public static class Day09
     private static readonly List<string> Lines = Util.ReadFile("/day09/input");
 
 
-    private static int Extrapolate(int[] numbers, int part = 1)
+    private static int Extrapolate(IReadOnlyList<int> numbers, int part = 1)
     {
         if (numbers.All(x => x == 0))
             return 0;
-        var nn = new int[numbers.Length - 1];
-        for (var i = 0; i < numbers.Length - 1; i++)
-            nn[i] = numbers[i + 1] - numbers[i];
-
+        var differences = numbers.Skip(1).Select((x, i) => x - numbers[i]).ToArray();
         if (part == 1)
-            return numbers[^1] + Extrapolate(nn); // part one
-        return numbers.First() - Extrapolate(nn,2); // part two
+            return numbers[^1] + Extrapolate(differences); // part one
+        return numbers[0] - Extrapolate(differences,2); // part two
     }
     
     public static int One => Lines.Sum(line => Extrapolate(line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()));
