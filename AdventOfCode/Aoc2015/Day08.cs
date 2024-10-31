05ac03aa76aa268b0db740e82e6e9e5d43db32e1
GeeGee <mohammedsouleymane@outlook.com>
2024-10-31 10:09:25 +0100

    day eight

--- AdventOfCode/Aoc2015/Day08.cs
+++ AdventOfCode/Aoc2015/Day08.cs
@@ -1,6 +1,10 @@
+using System.Text.RegularExpressions;
+
 namespace Aoc2015;
 
 public class Day08
 {
-    
+    private static readonly List<string> Literals = Util.ReadFile("/day08/input");
+    public static int NumberOfChars => Literals.Sum(x => x.Length -Regex.Unescape(x[1..^1]).Length);
+    public static int EncodedChars => Literals.Sum(x => 2+ x.Count(c => c is '\\' or '\"'));
 }
