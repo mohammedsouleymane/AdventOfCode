5c4623ef995ebd226c0f7c524229791a62e315bf
GeeGee <mohammedsouleymane@outlook.com>
2024-11-02 13:43:32 +0100

    day ten

--- AdventOfCode/Aoc2015/Day10.cs
+++ AdventOfCode/Aoc2015/Day10.cs
@@ -2,5 +2,18 @@ namespace Aoc2015;
 
 public class Day10
 {
+
     
+    public static long Expand()
+    {
+        var input = "3113322113";
+        for (var i = 0; i < 40; i++)
+        {
+            input = input.Select((x, i) => i > 0 && x != input[i - 1] ? "|" + x : x.ToString()).ToStr();
+            input = input.Split("|").Select(s =>  s.Length + s[0].ToString() ).ToStr();
+        }
+
+        return input.Length;
+
+    }
 }
