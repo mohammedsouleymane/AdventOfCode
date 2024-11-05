4b351d4a775110fc07c853fa7b0e1de57938dd1f
GeeGee <mohammedsouleymane@outlook.com>
2024-11-05 16:48:14 +0100

    update program.cs

---
+++ AdventOfCode/Aoc2015/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aoc2015;
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine(Day02.Surface());
+//Console.WriteLine(Day02.Ribbon());
+//Console.WriteLine(Day03.SoloVisited());
+//Console.WriteLine(Day03.RoboVisited());
+//Console.WriteLine(Day04.FiveLeadingZeros);
+//Console.WriteLine(Day04.SixLeadingZeros);
+//Console.WriteLine(Day05.AmountOfNice);
+//Console.WriteLine(Day05.AmountOfNiceWithoutErrors);
+//Console.WriteLine(Day06.HowManyAreLit());
+//Console.WriteLine(Day06.Brightness());
+//Console.WriteLine(Day07.Signal());
+//Console.WriteLine(Day07.Signal(two: true));
+//Console.WriteLine(Day08.NumberOfChars);
+//Console.WriteLine(Day08.EncodedChars);
+//Console.WriteLine(Day09.Solve());
+//Console.WriteLine(Day10.Expand());
+Console.WriteLine(Day11.NewPassword());
