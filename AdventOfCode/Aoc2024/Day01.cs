70ce10876c6f490045992db7103b45fb50c964ad
GeeGee <mohammedsouleymane@outlook.com>
2024-12-01 16:18:50 +0100

    udpate day01

--- AdventOfCode/Aoc2024/Day01.cs
+++ AdventOfCode/Aoc2024/Day01.cs
@@ -3,11 +3,8 @@ namespace Aoc2024;
 public static  class Day01
 {
     private static readonly List<string> Pairs = Util.ReadFile("/day01/input");
-    private static readonly List<int> Left = Pairs.Select(x => int.Parse(x.Split("  ")[0].Trim())).Order().ToList();
-    private static readonly List<int> Right = Pairs.Select(x => int.Parse(x.Split("  ")[1].Trim())).Order().ToList();
+    private static readonly List<int> Left = Pairs.Select(x => int.Parse(x.Split("  ").First().Trim())).Order().ToList();
+    private static readonly List<int> Right = Pairs.Select(x => int.Parse(x.Split("  ").Last().Trim())).Order().ToList();
     public static readonly int Difference = Left.Zip(Right).Sum( x => Math.Abs(x.First - x.Second));
     public static readonly int SimilarityScore = Left.Sum(x => Right.Count(y => x == y) * x);
-
-
-
 }
