e354c1a21fe40e470fc5c14db0fd44f8cdd2370b
GeeGee <mohammedsouleymane@outlook.com>
2025-12-05 10:47:48 +0100

    updated day05

--- AdventOfCode/Aoc2025/Day05.cs
+++ AdventOfCode/Aoc2025/Day05.cs
@@ -10,24 +10,27 @@ public static class Day05
         var ingredients = Input.Last().Select(long.Parse);
         return ingredients.Count(ingredient => ranges.Any(x => x.Item1 <= ingredient && x.Item2 >= ingredient));
     }
-
+    
     public static long ConsideredIds()
     {
-        var ranges = Input.First().Select(x=> x.Split("-")).Select(x=> (long.Parse(x[0]) , long.Parse(x[1])) ).OrderBy(x=> x.Item1).ThenBy(x=> x.Item2);
-        long sum = 0;
-
-        List<(long, long)> doneRanges = [];
-        foreach (var (l,r) in ranges)
+        var ranges = Input.First()
+            .Select(x=> x.Split("-"))
+            .Select(x=> (long.Parse(x[0]) , long.Parse(x[1])) )
+            .OrderBy(x=> x.Item1).ThenBy(x=> x.Item2)
+            .ToList();// sorts on start and then end of the ranges
+        var sum = ranges.First().Item2 - ranges.First().Item1 + 1; // initial range
+        var endOfBefore = ranges.First().Item2; 
+        
+        foreach (var (l,r) in ranges.Skip(1))
         {
-            if(doneRanges.Any(x=>  x.Item2 >= r )) // ignore because it's fully in another range (10 - 14 contains 12-13)
-                continue;
-            var overlap = doneRanges.Where(x => x.Item2 >= l).ToList(); // finds overlapping ranges
-            if (overlap.Count != 0)
-                sum += r - overlap.Last().Item2 ;
-            else
+            if(r <= endOfBefore) continue; // if the right hand is smaller or equal that means the ranges before fully contains the current one (10 - 14) contains (11 - 13)
+            if (l > endOfBefore) // if left hand if bigger than the ending range of before we just subtract r from l + 1
                 sum += r - l + 1;
-            doneRanges.Add((l,r));
-        }
+            else
+                sum += r - endOfBefore; // otherwise r - end of the range before
+
+            endOfBefore = r;
+        } // this all works because of the sorting
 
         return sum;
     }
