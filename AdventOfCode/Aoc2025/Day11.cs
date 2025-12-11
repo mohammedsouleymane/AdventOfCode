684615abe13971868ce78508b4e103bf7887c91f
GeeGee <mohammedsouleymane@outlook.com>
2025-12-11 09:04:05 +0100

    day 11 comments

--- AdventOfCode/Aoc2025/Day11.cs
+++ AdventOfCode/Aoc2025/Day11.cs
@@ -11,6 +11,7 @@ public static class Day11
         Cache.Clear();
         return Count(device, devices, end);
     }
+
     private static long Count(string device, Dictionary<string, string[]> devices, string end )
     {
         if (Cache.TryGetValue(device, out var count)) return count;
@@ -22,6 +23,7 @@ public static class Day11
         Cache.Add(device, sum);
         return sum;
     }
+
     public static long Paths(bool partOne = true)
     {
         Dictionary<string, string[]> devices = [];
@@ -29,10 +31,13 @@ public static class Day11
         {
             devices.Add(parsed[0], parsed[1].Split(" "));
         }
-
+        
         if (partOne) // part one
             return CountFresh("you", devices); // default from you to start
-        //divide into sub problems it looks like fft always comes before dac
+        //another solution is to keep the same structure as part 1 but cache device and i (counter for seeing fft and dac) Dict<(string,int),long> 
+        //i++ when device is fft or dac and only return 1 when i == 2
+        
+        //used solution: divide into sub problems it looks like fft always comes before dac
         return CountFresh("svr", devices, "fft") * // we count the paths from svr to fft
                CountFresh("fft", devices, "dac") * // then from fft to dac
                CountFresh("dac", devices); // dac to out by multiplying them we get the count
